51e6428b1872bc311a94e603129c6cf26bdaf9a1
Ryan Smith <smit7595@umn.edu>
2016-02-24 17:31:56 -0600

    Added blank constuctor and the ability to run with output only

--- teslamidi.py
+++ teslamidi.py
@@ -1,15 +1,31 @@
 import mido
 
 class TeslaCoilMidi:
+    def __init__(self):
+        self.inport = None
+        self.outport = None
     def __init__(self,inport_name,outport_name):
         self.inport = mido.open_input(inport_name)
         self.outport = mido.open_output(outport_name)
         self.outport.reset()
+    def set_input(self, port_name):
+        self.inport = mido.open_input(port_name)
+    def set_output(self, port_name):
+        self.outport = mido.open_output(port_name)
     def passthrough(self):
-        while True:
-            m = self.inport.receive()
-            self.outport.send(m)
+        if self.inport is not None and self.outport is not None:
+            while True:
+                m = self.inport.receive()
+                self.outport.send(m)
+        else:
+            raise TeslaCoilMidiException("An input port is required to use passthrough mode")
     def stop(self):
         self.inport.close()
         self.outport.reset()
         self.outport.close()
+
+class TeslaCoilMidiException(Exception):
+    def __init__(self, message):
+
+        # Call the base class constructor with the parameters it needs
+        super(ValidationError, self).__init__(message)
