b5d10ca56d9dd67172709203387819844a00b82f
Ryan Smith <smit7595@umn.edu>
2016-02-24 17:42:01 -0600

    Removed overloaded constructor

--- teslamidi.py
+++ teslamidi.py
@@ -4,10 +4,6 @@ class TeslaCoilMidi:
     def __init__(self):
         self.inport = None
         self.outport = None
-    def __init__(self,inport_name,outport_name):
-        self.inport = mido.open_input(inport_name)
-        self.outport = mido.open_output(outport_name)
-        self.outport.reset()
     def set_input(self, port_name):
         self.inport = mido.open_input(port_name)
     def set_output(self, port_name):
@@ -15,8 +11,12 @@ class TeslaCoilMidi:
     def passthrough(self):
         if self.inport is not None and self.outport is not None:
             while True:
-                m = self.inport.receive()
-                self.outport.send(m)
+                try:
+                    m = self.inport.receive()
+                    self.outport.send(m)
+                except KeyboardException:
+                    self.outport.reset()
+                    break
         else:
             raise TeslaCoilMidiException("An input port is required to use passthrough mode")
     def stop(self):
