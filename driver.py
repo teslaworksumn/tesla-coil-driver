9b36d6bc04cbeae8a5a8b0f26c42551c62711cc5
Ryan Smith <smit7595@umn.edu>
2016-02-24 17:36:36 -0600

    Added new passthrough macro and base help command

--- driver.py
+++ driver.py
@@ -1,7 +1,8 @@
 import argparse
 import atexit
-import sys
+import code
 import mido
+import sys
 
 from teslamidi import TeslaCoilMidi
 from midi_song import MidiSong as MS
@@ -11,7 +12,34 @@ parser.add_argument("-i", "--input", nargs="?", help="set the input MIDI port")
 parser.add_argument("-o", "--output", help="set the output MIDI port")
 args = parser.parse_args()
 
-tcm = TeslaCoilMidi(sys.argv[1],sys.argv[2])
+tcm = TeslaCoilMidi()
+
+if args.input is None:
+    sys.stderr.write("An input port is required to use passthrough mode\n")
+    tcm.set_output(args.output)
+else:
+    tcm.set_input(args.input)
+    tcm.set_output(args.output)
+
+if sys.flags.interactive:
+    print("Type `help()` to list commands.")
+    print(" Type `help(command)` for help on a specific command")
+else:
+    print("Type `help()` to list commands.")
+    print(" Type `help(command)` for help on a specific command")
+    code.interact(local=dict(globals(), **locals()))
+
+def help(command):
+    if command is None:
+        print("Available commands:")
+        print("  passthrough()      Passes the input midi directly to the output")
+    elif command is passthrough:
+        print("passthrough()")
+        print("  Passes the input midi directly to the output, effectively creating a software pipe between them")
+        print("  This is useful for attaching a keyboard to the comptuer; allowing the computer to interrput the signal.")
+
+def passthrough():
+    tcm.passthrough()
 
 def allstop():
     if tcm is not None:
