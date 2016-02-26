841bd8168a8330222b170e4d305732079ce28e4f
Ryan Smith <ryansmith125@gmail.com>
2016-02-25 20:31:51 -0600

    Added pause functionality

--- midi_song.py
+++ midi_song.py
@@ -9,15 +9,24 @@ class MidiSong:
         self.midifile = None
         self.midifile = MidiFile(self.filename)
         self.break_flag = False
+        self.paused = False
     def open(self):
         self.midifile = MidiFile(self.filename)
         self.server_thread = threading.Thread(target=self.run)
         self.server_thread.setName("Midi song: {0}".format(self.filename))
     def play(self):
-        self.break_flag = False
-        self.thread = threading.Thread(target=self.thread)
+        if self.paused:
+            self.pause()
+        else:
+            self.break_flag = False
+            self.paused = False
+            self.thread = threading.Thread(target=self.thread)
+    def pause(self):
+        self.paused = not self.paused
+        self.output.reset()
     def stop(self):
         self.break_flag = True
+        self.paused = False
         self.thread.join()
     def thread():
         for message in self.midifile.play():
